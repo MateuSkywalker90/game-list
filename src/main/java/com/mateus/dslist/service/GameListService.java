db1d28fde8430806ad61db2f0c275ab47c61dcc6
Mateus Oliveira <mateuskywalker90@gmail.com>
2025-01-21 14:32:47 -0400

    feat: created game list get all method

--- src/main/java/com/mateus/dslist/service/GameListService.java
+++ src/main/java/com/mateus/dslist/service/GameListService.java
@@ -1,12 +1,22 @@
 package com.mateus.dslist.service;
 
+import com.mateus.dslist.entity.GameList;
 import com.mateus.dslist.repository.GameListRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class GameListService {
 
     private final GameListRepository gameListRepository;
+
+    @Transactional(readOnly = true)
+    public List<GameList> findAll() {
+        return gameListRepository.findAll();
+    }
+
 }
--- src/main/java/com/mateus/dslist/web/controller/GameListController.java
+++ src/main/java/com/mateus/dslist/web/controller/GameListController.java
@@ -1,14 +1,27 @@
 package com.mateus.dslist.web.controller;
 
+import com.mateus.dslist.entity.GameList;
 import com.mateus.dslist.service.GameListService;
+import com.mateus.dslist.web.dto.GameListResponseDto;
+import com.mateus.dslist.web.dto.mapper.GameListMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/lists")
+@RequestMapping("api/v1/lists")
 public class GameListController {
 
     private final GameListService gameListService;
+
+    @GetMapping
+    public ResponseEntity<List<GameListResponseDto>> getAll() {
+        List<GameList> responseGameLists = gameListService.findAll();
+        return ResponseEntity.ok().body(GameListMapper.toGameListsDto(responseGameLists));
+    }
 }
